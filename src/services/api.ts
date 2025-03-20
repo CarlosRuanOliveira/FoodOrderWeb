ac5004c941cdd0b1a4bb1199359342885dbd4501
C.Ruan <carlosruan.ti@gmail.com>
2025-03-20 11:50:57 -0300

    refactor: Remove comments

--- src/services/api.ts
+++ src/services/api.ts
@@ -5,7 +5,7 @@ const api = axios.create({
   baseURL: 'http://localhost:8080/',
 });
 
-// Intercepta requisições para adicionar o token
+
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) {
@@ -14,14 +14,13 @@ api.interceptors.request.use((config) => {
   return config;
 });
 
-// Intercepta respostas para lidar com erro 401 (token expirado)
 api.interceptors.response.use(
   (response) => response,
   (error) => {
     if (error.response?.status === 401) {
       console.log('Token expirado, deslogando...');
-      logout(); // Chama a função de logout globalmente
-      window.location.href = '/login'; // Redireciona para a tela de login
+      logout();
+      window.location.href = '/login';
     }
     return Promise.reject(error);
   }
